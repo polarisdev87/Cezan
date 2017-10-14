f279d3e04ee159f6bb60d4847974de60fe47f531
polarisdev87 <polarisdev87@gmail.com>
2017-10-14 13:06:50 +0900

    initial commit

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+var config = {
+	apiKey: "AIzaSyA1VuGQI_JqdcZY8-LQER0CRjvR76oI8do",
+	authDomain: "cezan-1903a.firebaseapp.com",
+	databaseURL: "https://cezan-1903a.firebaseio.com",
+	projectId: "cezan-1903a",
+	storageBucket: "cezan-1903a.appspot.com",
+	messagingSenderId: "507918410853"
+};
+
+module.exports = config;
