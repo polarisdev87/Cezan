eed1f77499252590b7511efa85f789970655087f
polarisdev87 <polarisdev87@gmail.com>
2017-11-02 13:46:01 -0400

    update: firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,9 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({origin: true});
 const https = require("https");
-const request = require('request');
 const stripe = require("stripe")(
   "sk_test_2wl6eN9DurfaKXN8yuAtoAtl"
 );
-
-app.get("/download", (req, res) => {
-	const file_url = req.url.replace('/download?file=','');
-  https.get(file_url, function(response) {
-	  res.setHeader('Content-disposition', 'attachment; filename=' + 'resume.pdf');
-	  res.setHeader('Content-type', 'application/octet-stream');
-    response.pipe(res);
-  });
-})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
