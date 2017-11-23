6f5da5f252cbffe2233529ec2aec10941eb6c152
polarisdev87 <polarisdev87@gmail.com>
2017-11-23 17:41:37 -0500

    update: email notification

--- src/components/public/Published.jsx
+++ src/components/public/Published.jsx
@@ -115,7 +115,7 @@ class Published extends React.Component {
 
       if(!this.state.user || this.state.user.uid!== this.state.resume.uid) {
         // axios.get('https://geoip-db.com/json/').then((res) => {
-        axios.get('https://freegeoip.com/json/').then((res) => {
+        axios.get('https://freegeoip.net/json/').then((res) => {
           const location_data = res.data;
 
           let updates = {};
